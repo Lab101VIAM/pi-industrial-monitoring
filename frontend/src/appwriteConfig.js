9462e3c42b491c8f82d9e033ac795d16d1904aef
gitgian74 <gianluca@lab101.it>
2025-02-15 10:21:37 +0100

    aggiornamento files 3 miglioramento login page package src appwriteconfig

--- frontend/src/appwriteConfig.js
+++ frontend/src/appwriteConfig.js
@@ -1,7 +1,7 @@
 import { Client, Account } from 'appwrite';
 
 const client = new Client()
-  .setEndpoint('https://cloud.appwrite.io/v1')  // Sostituisci con il tuo endpoint
+  .setEndpoint('http://localhost/v1 ')  // Sostituisci con il tuo endpoint
   .setProject('67add2bf000f57426753');               // Sostituisci con il tuo Project ID
 
 export const account = new Account(client);
